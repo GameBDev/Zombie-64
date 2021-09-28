0827878c74c77c0a145aa985efad2af25d9b23b4
GameBDev <brendenylee@gmail.com>
2021-09-28 14:10:45 -0500

    added custom models

---
+++ Zombies 64/Assets/Scripts/Ativate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ativate : MonoBehaviour
+{
+    public GameObject thing;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            thing.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            thing.SetActive(false);
+        }
+    }
+}
