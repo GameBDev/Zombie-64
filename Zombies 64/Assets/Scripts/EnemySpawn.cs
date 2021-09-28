bfd9d3398da036e91d6732b7e901ce2bcb7960ee
GameBDev <brendenylee@gmail.com>
2021-09-27 19:42:06 -0500

    Nothing new

---
+++ Zombies 64/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject enemySpawn;
+  
+    void Update()
+    {
+        SpawnNewEnemey();
+    }
+    void SpawnNewEnemey()
+    {
+        
+    }
+}
